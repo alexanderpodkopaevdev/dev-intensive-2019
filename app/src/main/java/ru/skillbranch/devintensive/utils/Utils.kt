fc8a734241116edcf996dae24e57a6c1f3d12767
Alexander Podkopaev <p-alex1990@mail.ru>
2020-01-10 17:23:46 +0700

    correct module 3

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -3,9 +3,9 @@ package ru.skillbranch.devintensive.utils
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
         val names = fullName?.trim()?.split(" ")
-        val firstName = if (names?.getOrNull(0).isNullOrEmpty()) "null"
+        val firstName = if (names?.getOrNull(0).isNullOrEmpty()) null
         else names?.getOrNull(0)
-        val lastName = if (names?.getOrNull(1).isNullOrEmpty()) "null"
+        val lastName = if (names?.getOrNull(1).isNullOrEmpty()) null
         else names?.getOrNull(1)
         return firstName to lastName
     }
